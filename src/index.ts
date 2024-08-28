011c4f001cd65359e950378a24e88319d3aed3d5
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-08-27 22:59:55 -0300

    It's alive!!!!

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 require('dotenv').config()
+import express from 'express'
+import cors from 'cors'
 
-console.log(`Key = ${process.env.GEMINI_API_KEY}`);
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res, next) => {
+    res.json({ msg: "It's alive!!!!" })
+})
+
+app.listen(3000, () => {
+    console.log('CORS ativo. Web server rodando na porta 3000.')
+})
