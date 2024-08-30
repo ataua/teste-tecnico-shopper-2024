f185824df0eec5ca25bb9292e42fb291ae40baf2
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-08-30 11:49:47 -0300

    add prisma client

---
+++ src/prismaclient/prismaclient.ts
@@ -0,0 +1,7 @@
+// O arquivo foi deixado aqui porque o Prisma gerencia a pasta prisma/ e eu não quis misturar as coisas
+
+import { PrismaClient } from '@prisma/client'
+
+let prisma = new PrismaClient()
+
+export default prisma
