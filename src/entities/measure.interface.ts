c4ec39770085ad4a63346d5c165cbe5d2399c718
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-08-29 22:08:31 -0300

    update model measure

---
+++ src/entities/measure.interface.ts
@@ -0,0 +1,11 @@
+import { MeasureType } from "@prisma/client";
+
+export interface IMeasure {
+    image: string;
+    measure_uuid?: string;
+    measure_datetime: Date;
+    measure_value: number;
+    measure_type: MeasureType;
+    has_confirmed?: boolean;
+    customer_code: string;
+}
