b9ff558a5369351e0039cb9d8f065721908aa166
Atauã Pinali Doederlein <ataua.doederlein@gmail.com>
2024-09-02 15:51:13 -0300

    measure repository: create, findOne, update

--- src/repositories/measure.repository.ts
+++ src/repositories/measure.repository.ts
@@ -1,5 +1,6 @@
-import { IMeasure } from "@/entities/measure.interface"
-import prisma from "@/prismaclient/prismaclient"
+import { IMeasure } from "../entities/measure.d"
+import prisma from "../prismaclient/prismaclient"
+import { MeasureType } from "@prisma/client"
 
 const nullData = {
     measure_uuid: null,
@@ -14,7 +15,17 @@ const measureRepository = {
 
     create: async (measure: IMeasure) => {
         try {
+            measure.measure_type = measure.measure_type.toLocaleUpperCase() as MeasureType
             return await prisma.measure.create({ data: measure })
+        } catch (error: any) {
+            console.error({ error })
+            return nullData
+        }
+    },
+
+    findOne: async (measure_uuid: string) => {
+        try {
+            return await prisma.measure.findUnique({ where: { measure_uuid } }) || nullData
         } catch (error: any) {
             return nullData
         }
@@ -34,12 +45,26 @@ const measureRepository = {
             return await prisma.measure.findFirst({
                 where: {
                     measure_datetime,
-                    measure_type,
+                    measure_type: measure_type!.toLocaleUpperCase() as MeasureType,
                     customer_code
                 }
             })
         } catch (error: any) {
             return { data: {}, error: error.message }
+        }
+    },
+
+    update: async (measure_uuid: string, measure_value: number) => {
+        try {
+            return await prisma.measure.update({
+                where: { measure_uuid },
+                data: {
+                    has_confirmed: true, 
+                    measure_value
+                }
+            })
+        } catch (error: any) {
+            return nullData
         }
     }
 }
